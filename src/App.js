faea771adcb03482ed01ad1590f37c630a0f6d9d
Rob Brown <rabaroni@gmail.com>
2021-03-28 18:03:32 -0400

    Baby's 1st API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [cycles, setCycles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://netrunnerdb.com/api/2.0/public/cycles")
+      .then(res => res.json())
+      .then(response => {
+        setCycles(response.data);
+        setIsLoading(false);
+      })
+    ;
+  })
+
   return (
     <div className="App">
+      {isLoading && <h1>Loading...</h1>}
 
+      {cycles.map((cycle, index) => (
+        <h3 key={index}>{cycle.name}: {cycle.code}</h3>
+      ))}
     </div>
   );
 }
