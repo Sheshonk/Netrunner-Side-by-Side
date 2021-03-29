fc5cf9844602e7a5fb6a737de50a6acaa40e2187
Rob Brown <rabaroni@gmail.com>
2021-03-29 19:42:01 -0400

    side switch

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   const [cards, setCards] = useState([]);
   const [cycles, setCycles] = useState([]);
+  const [factions, setFactions] = useState([]);
+  const [selectedFaction, setSelectedFaction] = useState("");
   const [selectedSide, setSelectedSide] = useState("corp");
   const [sides, setSides] = useState([]);
 
-  const bannedCards = ["30077"]; //TODO: add more
+  const bannedCards = [/*Catalyst*/ "30076", /*Syndicate*/ "30077"]; //TODO: add more
   const packs = ["sg", "su21", "df", "ur"];
 
   useEffect(() => {
@@ -25,6 +27,13 @@ function App() {
       })
     ;
 
+    fetch("https://netrunnerdb.com/api/2.0/public/factions")
+      .then(res => res.json())
+      .then(response => {
+        setFactions(response.data);
+      })
+    ;
+
     fetch("https://netrunnerdb.com/api/2.0/public/sides")
       .then(res => res.json())
       .then(response => {
@@ -33,13 +42,21 @@ function App() {
     ;
   }, [])
 
+  function OnChangeFaction(event) {
+    setSelectedFaction(event.target.value);
+  }
+
   function OnChangeSide(event) {
+    setSelectedFaction("");
     setSelectedSide(event.target.value);
   }
 
   return (
     <div className="App">
-      <span>Side: </span>
+      <span>Format: </span>
+      <select></select>
+
+      <span>&emsp;Side: </span>
       <select onChange={OnChangeSide} value={selectedSide}>
         {
           sides.sort((a, b) => a.name.localeCompare(b.name)).map((side, index) => (
@@ -50,26 +67,28 @@ function App() {
       
 
       <span>&emsp;Faction: </span>
-      <select>
-        <option value="A">A</option>
-        <option value="B">B</option>
+      <select onChange={OnChangeFaction} value={selectedFaction}>
+        <option value={null}>All</option>
+        {
+          factions.filter(faction => faction.side_code === selectedSide).sort((a, b) => a.name.localeCompare(b.name)).map((faction, index) => (
+            <option value={faction.code}>{faction.name}</option>
+          ))
+        }
       </select>
-
-      <span>&emsp;{selectedSide}</span>
-
+  
       <hr />
 
       <h2>Identites</h2>
-      {cards.filter(card => card.side_code === "corp" && card.type_code === "identity" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
+      {cards.filter(card => card.side_code === selectedSide && card.type_code === "identity" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
         <img alt={card.title} className="card-image" key={index} src={`https://netrunnerdb.com/card_image/large/${card.code}.jpg`} />
       ))}
 
-      <h2>Agendas</h2>
-      {cards.filter(card => card.side_code === "corp" && card.type_code === "agenda" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || b.agenda_points - a.agenda_points || a.advancement_cost - b.advancement_cost || a.title.localeCompare(b.title)).map((card, index) => (
+      {selectedSide === "corp" ? <h2>Agendas</h2> : (null)}
+      {cards.filter(card => card.side_code === selectedSide && card.type_code === "agenda" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || b.agenda_points - a.agenda_points || a.advancement_cost - b.advancement_cost || a.title.localeCompare(b.title)).map((card, index) => (
         <img alt={card.title} className="card-image" key={index} src={`https://netrunnerdb.com/card_image/large/${card.code}.jpg`} />
       ))}
 
-      <h2>Operations</h2>
+      {selectedSide === "corp" ? <h2>Operations</h2> : (null)}
       {/*
       Econ
       Card Draw
@@ -81,11 +100,11 @@ function App() {
 
       Other
       */}
-      {cards.filter(card => card.side_code === "corp" && card.type_code === "operation" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
+      {cards.filter(card => card.side_code === selectedSide && card.type_code === "operation" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
         <img alt={card.title} className="card-image" key={index} src={`https://netrunnerdb.com/card_image/large/${card.code}.jpg`} />
       ))}
 
-      <h2>Assets</h2>
+      {selectedSide === "corp" ? <h2>Assets</h2> : (null)}
       {/*
       Econ
       Card Draw
@@ -97,23 +116,23 @@ function App() {
 
       Other
       */}
-      {cards.filter(card => card.side_code === "corp" && card.type_code === "asset" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
+      {cards.filter(card => card.side_code === selectedSide && card.type_code === "asset" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
         <img alt={card.title} className="card-image" key={index} src={`https://netrunnerdb.com/card_image/large/${card.code}.jpg`} />
       ))}
 
-      <h2>Upgrades</h2>
-      {cards.filter(card => card.side_code === "corp" && card.type_code === "upgrade" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
+      {selectedSide === "corp" ? <h2>Upgrades</h2> : (null)}
+      {cards.filter(card => card.side_code === selectedSide && card.type_code === "upgrade" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
         <img alt={card.title} className="card-image" key={index} src={`https://netrunnerdb.com/card_image/large/${card.code}.jpg`} />
       ))}
 
-      <h2>Ice</h2>
+      {selectedSide === "corp" ? <h2>Ice</h2> : (null)}
       {/*
       Barrier
       Code Gate
       Sentry
       Other
       */}
-      {cards.filter(card => card.side_code === "corp" && card.type_code === "ice" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
+      {cards.filter(card => card.side_code === selectedSide && card.type_code === "ice" && packs.includes(card.pack_code) && !bannedCards.includes(card.code)).sort((a, b) => a.faction_code.localeCompare(b.faction_code) || a.title.localeCompare(b.title)).map((card, index) => (
         <img alt={card.title} className="card-image" key={index} src={`https://netrunnerdb.com/card_image/large/${card.code}.jpg`} />
       ))}
     </div>
